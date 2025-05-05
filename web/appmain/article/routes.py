ee903572a0fe57914938fb60d4fa01016ca88acc
snuny2 <82195651+snuny2@users.noreply.github.com>
2025-05-05 13:39:35 +0900

    edit article page

--- web/appmain/article/routes.py
+++ web/appmain/article/routes.py
@@ -99,3 +99,67 @@ def getRecentArticles():
                 payload = {"success": True, "articles": recentArticleDics}
 
                 return make_response(jsonify(payload), 200)
+
+@article.route('/display_article/<int:articleNo>', methods=['GET']) #상품번호로 접속해주는 코드
+def displayArticle(articleNo):
+    return send_from_directory(app.root_path, 'templates/display_article.html')
+
+def translateCategory(catId):
+    category = '미분류'
+
+    if catId == 0:
+        category = '인문'
+    elif catId == 1:
+        category = '사회과학'
+    elif catId == 2:
+        category = '자연과학'
+    elif catId == 3:
+        category = '의학'
+    elif catId == 4:
+        category = '경제/경영'
+    elif catId == 5:
+        category = '공학'
+    elif catId == 6:
+        category = '음악'
+    elif catId == 7:
+        category = '미술'
+    elif catId == 8:
+        category = '기타'
+    else:
+        category = '미분류'
+
+    return category
+
+@article.route('/api/article/display', methods=['GET', 'POST'])
+def displayArticle():
+
+    data = request.form
+    articleNo = data.get("articleNo")
+
+    payload = {"success": False}
+
+    conn = sqlite3.connect('pyBook.db')
+    cursor = conn.cursor()
+
+    if cursor:
+        SQL = 'select author, title, category, description, price, picture from articles where articleNo = ?'
+        cursor.execute(SQL, (articleNo,))
+        result = cursor.fetchone()
+
+        cursor.close()
+        conn.close()
+
+    if result:
+        if result[5]:
+            picFilePath = 'pics/' + result[0] + '/' + result[5]
+            picURL = url_for('static', filename=picFilePath, _external=True)
+        else:
+            picURL = None
+
+        article = {"author": result[0], "title": result[1], "category": translateCategory(result[2]),
+                   "description": result[3], "price": result[4], "picture": picURL}
+        payload = {"success": True, "articles": article}
+    else:
+        payload = {"success": True, "articles": None}
+
+        return make_response(jsonify(payload), 200)
